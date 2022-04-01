2a0a063703b1deeb4eb922f05c562ba76fa11632
veronicapava <veronicapava@gmail.com>
2022-04-01 00:41:54 -0500

    Arreglo de Cors

--- back/src/main/java/com/sofka/ferreteria/FerreteriaApplication.java
+++ back/src/main/java/com/sofka/ferreteria/FerreteriaApplication.java
@@ -2,7 +2,10 @@ package com.sofka.ferreteria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableReactiveMongoRepositories
@@ -10,6 +13,16 @@ public class FerreteriaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FerreteriaApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry){
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
