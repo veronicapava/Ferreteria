c811f995465387916f045019be831688f247d51a
veronicapava <veronicapava@gmail.com>
2022-04-01 21:42:30 -0500

    Logica para crear nuevo articulo

--- front/src/components/Pages/Inventory.jsx
+++ front/src/components/Pages/Inventory.jsx
@@ -4,8 +4,10 @@ import Article from "../Atoms/Article"
 import Header from "../Header"
 
 const Inventory = () => {
-  //TODO: añadir nuevo articulo
   const [items, setItems] = useState([])
+  const [nuevoProd, setNuevoProd] = useState({})
+  const [creando, setCreando] = useState(false)
+
   useEffect(() => {
     async function fetching() {
       let data = await fetchApi("/inventario/lista")
@@ -15,16 +17,58 @@ const Inventory = () => {
     fetching()
   }, [])
 
+  const crearArticle = async () => {
+    let request = await fetchApi(`/inventario/crear`, "POST", nuevoProd)
+    let newProductData = await fetchApi(`/inventario/${request.id}`)
+    console.log(request)
+    setItems([...items, newProductData])
+    setCreando(false)
+  }
+
   return (
     <div className="container mt-5">
       <Header />
       <h1>Inventario</h1>
       <h1>Articulos en inventario</h1>
-      <div className="ed-container">
-        {items.map((item) => (
-          <Article item={item} key={item.id} />
-        ))}
-      </div>
+
+      <button onClick={() => setCreando(true)}>Crear nuevo articulo</button>
+      {creando ? (
+        <article>
+          <input
+            type="text"
+            placeholder="nombreprod"
+            onChange={(e) => setNuevoProd({ ...nuevoProd, nombreProducto: e.target.value })}
+          />
+          <input
+            type="number"
+            placeholder="preciounid"
+            onChange={(e) => setNuevoProd({ ...nuevoProd, precioUnd: e.target.value })}
+          />
+          <input
+            type="number"
+            placeholder="cantidad en bodega"
+            onChange={(e) => setNuevoProd({ ...nuevoProd, cantidadEnBodega: e.target.value })}
+          />
+          <input
+            type="number"
+            placeholder="cantidad maxima "
+            onChange={(e) => setNuevoProd({ ...nuevoProd, cantidadMax: e.target.value })}
+          />
+          <input
+            type="number"
+            placeholder="cantidad minima"
+            onChange={(e) => setNuevoProd({ ...nuevoProd, cantidadMin: e.target.value })}
+          />
+          <button onClick={() => crearArticle()}>Guardar</button>
+          <button onClick={() => setCreando(false)}>Cancelar</button>
+        </article>
+      ) : (
+        <div className="container">
+          {items.map((item) => (
+            <Article item={item} key={item.id} />
+          ))}
+        </div>
+      )}
     </div>
   )
 }
